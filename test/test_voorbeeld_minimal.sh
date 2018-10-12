8382ddd3d7f87b404feb914b18536e70fbd7db4f
Thijs Brentjens <thijs@brentjensgeoict.nl>
2018-10-12 16:10:16 +0200

    werk in uitvoering: Service profiel 2.0 INSPIRE variant

--- test/test_voorbeeld_minimal.sh
+++ test/test_voorbeeld_minimal.sh
@@ -1,2 +1,3 @@
 sh prepareSchematronDataset20.sh
+# example call
 saxonb-xslt /home/thijs/code/Geonovum/ETF/datavalidatie/metadata/profiel2.0/voorbeeld-dataset_zonder_MD_RepresentativeFraction.xml theSchema.xsl
--- test/test_voorbeeld_v2-uitgebreid.sh
+++ test/test_voorbeeld_v2-uitgebreid.sh
@@ -1,2 +1,3 @@
 sh prepareSchematronDataset20.sh
+# example call
 saxonb-xslt /home/thijs/code/Geonovum/ETF/datavalidatie/metadata/profiel2.0/voorbeeld-dataset.xml theSchema.xsl
