6147d1d353a6be6aefe0b53b0626266caad4f196
Thijs Brentjens <thijs@brentjensgeoict.nl>
2018-10-15 14:53:58 +0200

    syntax fixes in Schematron code voor thesaurus, weghalen test uuidref en referenceSystemInfo (optioneel)

--- test/test_voorbeeld_minimal.sh
+++ test/test_voorbeeld_minimal.sh
@@ -1,3 +1,3 @@
 sh prepareSchematronDataset20.sh
 # example call
-saxonb-xslt /home/thijs/code/Geonovum/ETF/datavalidatie/metadata/profiel2.0/voorbeeld-dataset_zonder_MD_RepresentativeFraction.xml theSchema.xsl
+saxonb-xslt /home/thijs/code/Geonovum/ETF/datavalidatie/metadata/profiel2.0/Voorbeeld_Metadata_Dataset_2017_geldig_Ine.xml theSchema.xsl
---
+++ test/test_voorbeeld_service.sh
@@ -0,0 +1,3 @@
+sh prepareSchematronServices20.sh
+# example call
+saxonb-xslt /home/thijs/code/Geonovum/ETF/datavalidatie/metadata/profiel2.0/Voorbeeld_Metadata_Services_2017.xml theSchemaSrv.xsl
